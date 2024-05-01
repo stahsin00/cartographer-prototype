b0b5264d989135f7a7088eb1e8a90d0fb2d874d7
stahsin00 <lamisa1998@hotmail.com>
2024-05-01 00:24:00 -0700

    render ship

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -6,11 +6,23 @@ public class Ship
 
     public Ship (int size) {
         Size = size;
+        tiles = new Tile [Size,Size];
 
         for (int x = 0; x < Size; x++) {
             for (int y = 0; y < Size; y++) {
                 tiles [x, y] = new Tile(x, y);
             }
+        }
+    }
+
+    public Tile GetTile(int x, int y) {
+        return tiles [x, y];
+    }
+
+    public Tile this[int x, int y] {
+        get 
+        { 
+            return tiles [x, y];
         }
     }
 }
--- Assets/Scripts/WorldController.cs
+++ Assets/Scripts/WorldController.cs
@@ -6,7 +6,11 @@ public class WorldController : MonoBehaviour
 {
     public const int SHIP_SIZE = 16;
 
+    public Sprite floorSprite;
+
     public Ship ship;
+
+    Dictionary<Tile, GameObject> tileMap = new Dictionary<Tile, GameObject>();
 
     private static WorldController instance;
     public static WorldController Instance { get { return instance; } }
@@ -24,6 +28,26 @@ public class WorldController : MonoBehaviour
             DontDestroyOnLoad(gameObject);
 
             ship = new Ship(SHIP_SIZE);
+            RenderShip();
+        }
+    }
+
+    private void RenderShip() {
+        for (int x = 0; x < ship.Size; x++) {
+            for (int y = 0; y < ship.Size; y++) {
+                Tile tile = ship[x,y];
+
+				GameObject tileGO = new GameObject();
+
+				tileMap.Add(tile, tileGO);
+
+				tileGO.name = "Tile_" + x + "_" + y;
+				tileGO.transform.position = new Vector3(tile.X, tile.Y, 0);
+				tileGO.transform.SetParent(this.transform, true);
+
+				SpriteRenderer sr = tileGO.AddComponent<SpriteRenderer>();
+				sr.sprite = floorSprite;
+            }
         }
     }
 }
