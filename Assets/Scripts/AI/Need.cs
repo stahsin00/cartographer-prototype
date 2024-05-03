68bc90eafbce207351945f248e501146dcfacee1
stahsin00 <lamisa1998@hotmail.com>
2024-05-03 15:41:25 -0700

    refactor need class

--- Assets/Scripts/AI/Need.cs
+++ Assets/Scripts/AI/Need.cs
@@ -1,7 +1,19 @@
+using System;
+
 public abstract class Need
 {
     public string Type { get; protected set; }
-    public float Current { get; protected set; }
+
+    private float current;
+    public float Current 
+    {
+        get => current;
+        protected set => current = Math.Clamp(value, 0, 1);
+    }
+
+    public virtual void Recover(float amount) {
+        Current += amount;
+    }
 
     public abstract void Decay();
 
